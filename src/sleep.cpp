59b01818a1a14df3ab341512fb8834530d56800d
Nick Yonge <nick@curiobot.com>
2024-10-19 15:33:00 -0700

    functional sleep timer :)

--- src/sleep.cpp
+++ src/sleep.cpp
@@ -1,9 +1,9 @@
 #include "sleep.h"
 
 #ifdef USE_SLEEP_TIMER
-
-byte seconds_idle = 0;
-byte minutes_idle = 0;
+int loopsIdle = 0;
+byte secondsIdle = 0;
+byte minutesIdle = 0;
 #endif
 
 void setupSleep()
@@ -16,29 +16,65 @@ void setupSleep()
 void loopSleep()
 {
 #ifdef USE_SLEEP_TIMER
-    secondInterval;
+    // increment idle timer
+    loopsIdle++;
+    if (loopsIdle >= secondInterval)
+    {
+        // increment seconds and minutes as needed
+        loopsIdle = 0;
+        secondsIdle++;
+        if (secondsIdle >= SECONDS_MAX)
+        {
+            // seconds overflow, increment minute
+            secondsIdle = 0;
+            minutesIdle++;
+            // ensure minutesIdle does not exceed given limit
+            if (minutesIdle > MINUTES_MAX)
+            {
+                minutesIdle = MINUTES_MAX;
+            }
+        }
+        // check for sleep timer limits
+        if (minutesIdle >= MINUTES_UNTIL_SLEEP && secondsIdle >= SECONDS_UNTIL_SLEEP)
+        {
+            // device has reached sleep mode timeout
+            goToSleep();
+        }
+    }
 #endif
 }
 
 void resetSleepTimer()
 {
 #ifdef USE_SLEEP_TIMER
+    loopsIdle = 0;
+    secondsIdle = 0;
+    minutesIdle = 0;
 #endif
 }
 
 void goToSleep()
 {
-    sleepInput();        // put input system to sleep
-    sleepLEDs();         // put LED display to sleep
+    // 1) Sleep other classes as necessary
+    sleepInput(); // put input system to sleep
+    sleepLEDs();  // put LED display to sleep
+
+    // 2) Prep device for sleep mode
+    resetSleepTimer();   // reset sleep timing values
     sleep_enable();      // enable sleep bit
     sleep_bod_disable(); // disable brownout detection
     sei();               // ensure interrupts are active
-    sleep_cpu();         // begin sleep mode
+
+    // 3) put device to sleep
+    sleep_cpu(); // begin sleep mode
 
     // >>> device automatically wakes on any interrupt <<<
 
+    // 4) Device woken up by interrupt, disable sleep mode
     sleep_disable(); // disable sleep bit
     sei();           // re-enable interrupts again (failsafe)
-    wakeInput();     // wake up input system
-    wakeLEDs();      // wake up LED display
+
+    // 5) Wake other classes as necessary
+    wakeInput(); // wake up input system
+    wakeLEDs();  // wake up LED display
 }
--- src/sleep.h
+++ src/sleep.h
@@ -4,27 +4,41 @@
 #include <Arduino.h>
 #include <avr/sleep.h>
 
-#include "main.h"
-#include "input.h"
+#include "main.h" // note: importing main also imports other classes needed to call wake/sleep
 
 #define USE_SLEEP_TIMER
 
 #ifdef USE_SLEEP_TIMER
 
-// one second, taking DELAY_INTERVAL into account
+// one second, taking main.h DELAY_INTERVAL into account
 constexpr int secondInterval = 1000 / DELAY_INTERVAL;
 
-#define SECONDS_UNTIL_SLEEP 5
-#define MINUTES_UNTIL_SLEEP 0
+#define SECONDS_MAX 60 // 60s = 1 minute (it'd be bonkers to change this, but hey, maybe you just hate counting in Base60?)
+#define MINUTES_MAX 240// 240mins = 4 hours (prevent byte overflow at 255)
+
+#define MINUTES_UNTIL_SLEEP 0 // how many minutes (plus given seconds) until device goes to sleep?
+#define SECONDS_UNTIL_SLEEP 5 // how many seconds (after minutes target is reached) until device goes to sleep?
 
 #endif // USE_SLEEP_TIMER
 
 void setupSleep();
 void loopSleep();
 
-// resets the countdown timer for the device going to sleep
+// Resets the countdown timer for the device going to sleep
 void resetSleepTimer();
-// puts device to sleep, until any interrupt is received
+// Puts device to sleep, until any interrupt is received.
+// NOTE: if no interrupts are set up, THIS FUNCTIONALLY DISABLES THE DEVICE. BE CAREFUL!
 void goToSleep();
+
+// error checks for defined values
+#if MINUTES_UNTIL_SLEEP > MINUTES_MAX
+#error "Max minutes exceeds minutes until sleep, device will never get a chance to rest!"
+#endif
+#if SECONDS_UNTIL_SLEEP > SECONDS_MAX
+#error "Seconds until sleep exceeds max seconds increment (which should be 60), device will never get a chance to rest!"
+#endif
+#if MINUTES_MAX > 255 || SECONDS_MAX > 255 || MINUTES_UNTIL_SLEEP > 255 || SECONDS_UNTIL_SLEEP > 255
+#error "Minutes and/or seconds sleep timers and/or max limits exceed 255. They are byte values, ensure they're 255 or less!"
+#endif
 
 #endif // SLEEP_H
