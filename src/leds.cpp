3895060f2b3bb1dc81040e38db52056d8e66c8d5
Nick Yonge <nick@curiobot.com>
2024-10-19 14:39:19 -0700

    built out Fire2012 anim, too large, moved anim into DISABLE_ANIM flag

--- src/leds.cpp
+++ src/leds.cpp
@@ -5,9 +5,14 @@ static bool savedLEDsThisSession = false; // on first save, ensure LEDs are upda
 CRGB leds[NUM_LEDS];
 static int ledColor = DATA_DEFAULT_LED_HUE; // current LED HSV hue
 
+static CRGB colorsArray[NUM_LEDS];
+
+#ifndef DISABLE_ANIMATION
+bool clearLEDs = false;
 bool animate = true;
 bool reverseAnimDirection = false;
 int animTimer = 0;
+#endif
 
 void setupLEDs()
 {
@@ -27,6 +32,7 @@ void setupLEDs()
 
 void loopLEDs()
 {
+#ifndef DISABLE_ANIMATION
     // run animation
     if (animate)
     {
@@ -39,14 +45,38 @@ void loopLEDs()
             animateLEDs();
         }
     }
+#endif
 }
 
 void updateLEDs()
 {
+#ifndef DISABLE_ANIMATION
+    // if (clearLEDs)
+    // {
+    //     for (int i = 0; i < NUM_LEDS; i++)
+    //     {
+    //         colorsArray[i] = CRGB::Black;
+    //     }
+    //     clearLEDs = false;
+    // }
+    // else if (!animate)
+    if (!animate)
+    {
+        for (int i = 0; i < NUM_LEDS; i++)
+        {
+            colorsArray[i] = CRGB(CHSV(ledColor, 255, 255));
+        }
+    }
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        leds[i] = colorsArray[i];
+    }
+#else
     for (int i = 0; i < NUM_LEDS; i++)
     {
         leds[i] = CRGB(CHSV(ledColor, 255, 255));
     }
+#endif
     FastLED.show();
 }
 
@@ -65,9 +95,49 @@ void shiftLEDColor(int delta)
     saveLEDData();
 }
 
+#ifndef DISABLE_ANIMATION
 void animateLEDs()
 {
+    random16_add_entropy(random());
+    // Array of temperature readings at each simulation cell
+    static uint8_t heat[NUM_LEDS];
+
+    // Step 1.  Cool down every cell a little
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for (int k = NUM_LEDS - 1; k >= 2; k--)
+    {
+        heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+    }
+
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if (random8() < SPARKING)
+    {
+        int y = random8(7);
+        heat[y] = qadd8(heat[y], random8(160, 255));
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for (int j = 0; j < NUM_LEDS; j++)
+    {
+        // Scale the heat value from 0-255 down to 0-240
+        int pixelnumber;
+        if (reverseAnimDirection)
+        {
+            pixelnumber = (NUM_LEDS - 1) - j;
+        }
+        else
+        {
+            pixelnumber = j;
+        }
+        colorsArray[pixelnumber] = CRGB(CHSV(ledColor, 255, heat[j]));
+    }
 }
+#endif
 
 void clearLEDLocalData()
 {
@@ -76,12 +146,9 @@ void clearLEDLocalData()
     // FastLED.clear(true);
     // NOTE: FastLED.clear(true) seems to add add'l size to the build, causing it to fail.
     //       Even tho the below is functionally identical, this does NOT break the build.
-    for (int i = 0; i < NUM_LEDS; i++)
-    {
-        leds[i] = CRGB::Black;
-    }
-    FastLED.show();
-    FastLED.clearData();
+    // clearLEDs = true;
+    // updateLEDs();
+    // FastLED.clearData();
 }
 
 void loadLEDData()
--- src/leds.h
+++ src/leds.h
@@ -12,19 +12,32 @@
 #define RGB_ORDER GRB
 
 #define NUM_LEDS 12
-
 #define LED_BRIGHTNESS 120
-#define ANIM_FPS 30
 
+#define DISABLE_ANIMATION
+
+#ifndef DISABLE_ANIMATION
+#define ANIM_FPS 30
 constexpr int animInterval = (1000 / ANIM_FPS) / DELAY_INTERVAL;
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 55, suggested range 20-100
+#define COOLING 55
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 120
+#endif
 
 void setupLEDs();
 void loopLEDs();
 
 void shiftLEDColor(int delta); // shift the current LED colour by the given amount
 void updateLEDs();
+#ifndef DISABLE_ANIMATION
 // LED animation sourced from FastLED's Fire2012
 void animateLEDs();
+#endif
 
 void clearLEDLocalData();
 void loadLEDData();
