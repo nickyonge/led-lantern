a7f0a8cc630d2a8046387d7314b3ab5052078ce7
Nick Yonge <nick@curiobot.com>
2024-10-28 20:26:50 -0700

    fixed byteMath header definitions

--- src/byteMath.h
+++ src/byteMath.h
@@ -3,13 +3,17 @@
 
 #include <Arduino.h>
 
-// Subtracts byte `subtract` from given byte `value`, returning the result, WITHOUT OVERFLOW. Capped to 0-255
-byte subtractByte(byte value, byte subtract);
-// Subtracts int `subtract` from given byte `value`, returning the result, WITHOUT OVERFLOW. Capped to 0-255
-byte subtractByte(byte value, int subtract);
-// Adds byte `subtract` to given byte `value`, returning the result, WITHOUT OVERFLOW. Capped to 0-255
-byte addByte(byte value, byte add);
-// Adds int `subtract` to given byte `value`, returning the result, WITHOUT OVERFLOW. Capped to 0-255
-byte addByte(byte value, int add);
+// Subtracts byte `subtract` from given byte `value`, returning the result, WITHOUT UNDERFLOW. 
+// Capped to `minValue`-255, default 'minValue = 0'
+byte subtractByte(byte value, byte subtract, byte minValue = 0);
+// Subtracts int `subtract` from given byte `value`, returning the result, WITHOUT OVER/UNDERFLOW. 
+// Capped to `minValue`-255, default 'minValue = 0'
+byte subtractByte(byte value, int subtract, byte minValue = 0);
+// Adds byte `subtract` to given byte `value`, returning the result, WITHOUT OVERFLOW. 
+// Capped to 0-`maxValue`, default `maxValue = 255`
+byte addByte(byte value, byte add, byte maxValue = 255);
+// Adds int `subtract` to given byte `value`, returning the result, WITHOUT OVER/UNDERFLOW.
+// Capped to 0-`maxValue`, default `maxValue = 255`
+byte addByte(byte value, int add, byte maxValue = 255);
 
 #endif // BYTEMATH_H
